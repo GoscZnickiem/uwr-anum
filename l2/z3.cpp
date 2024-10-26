9a7483a7d3d100cc1acbaadd0468a0cbe819ebd0
GoscZnickiem <maciek.6098@gmail.com>
2024-10-26 20:27:55 +0200

    a

--- l2/z3.cpp
+++ l2/z3.cpp
@@ -8,7 +8,7 @@ int main() {
 	for(int i = 0; i < 1 << 4; i++) {
 		for(float j = 0.5f; j <= 2; j *= 2) {
 			for(int k = -1; k < 2; k += 2) {
-				float num = k * static_cast<float>((1 << 5) + i) * j / (1 << 5);
+				float num = k * static_cast<float>((1 << 4) + i) * j / (1 << 5);
 				auto pos = std::lower_bound(numbers.begin(), numbers.end(), num);
 				numbers.insert(pos, num);
 			}
